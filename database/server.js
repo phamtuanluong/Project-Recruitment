aa770073c9f05458159458d04190b3eab81c32a2
Tuan Luong <tuanluongkt1907@gmail.com>
2025-02-17 23:31:43 +0700

    Add recruitment

--- database/server.js
+++ database/server.js
@@ -1,12 +1,16 @@
 const jsonServer = require("json-server");
+const path = require("path");
+
 const server = jsonServer.create();
-const router = jsonServer.router("database.json"); // Kết nối với database
+const router = jsonServer.router(path.join(__dirname, "database.json")); 
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
 });
+
+
