472bd357d5fc3bf72d43ff2f4e48d474154c1838
Tuan Luong <tuanluongkt1907@gmail.com>
2025-02-17 23:29:30 +0700

    Add recruitment

--- database/server.js
+++ database/server.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json"); // Kết nối với database
+const router = jsonServer.router("database.json"); // Kết nối với database
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
